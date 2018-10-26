fd129aa69f095aba19ec05b9938575f2127798ea
lgv2016 <lgv2016@qq.com>
2018-10-26 21:28:03 +0800

    添加了bsp_dma.h文件

---
+++ BSP/bsp_dma.h
@@ -0,0 +1,15 @@
+#ifndef __BSP_DMA_H
+#define __BSP_DMA_H
+
+#include <stm32f4xx.h>
+
+#define DMA_Dbus_Buff_Size 18
+#define DMA_Judge_Send_Buff_Size 255
+#define DMA_Judge_Reve_Buff_Size 255
+
+extern u8 g_DMA_Dbus_Buff[DMA_Dbus_Buff_Size];
+extern u8 g_DMA_Judge_Send_Buff[DMA_Judge_Send_Buff_Size];
+extern u8 g_DMA_Judge_Reve_Buff[DMA_Judge_Reve_Buff_Size];
+
+extern void BSP_DMA_Init(void);
+#endif  
