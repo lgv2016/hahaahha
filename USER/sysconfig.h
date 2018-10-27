07f7e6b260c3ced4020cf70e56605dad49ab2d03
lgv2016 <lgv2016@qq.com>
2018-10-27 14:26:58 +0800

    添加了bsp_time.h头文件

--- USER/sysconfig.h
+++ USER/sysconfig.h
@@ -1,6 +1,7 @@
 #ifndef __SYSCONFIG_H
 #define __SYSCONFIG_H
 
+
 #include <stdio.h>
 #include <delay.h>
 #include <bsp_gpio.h>
@@ -8,7 +9,7 @@
 #include <bsp_dma.h>
 #include <bsp_nvic.h>
 #include <bsp_can.h>
-
+#include <bsp_time.h>
 
 
 extern void SYS_Config_Init(void);
