1ea6fe0717ad0940c0de239c6078ce6f1ccc9d3b
lgv2016 <lgv2016@qq.com>
2018-10-28 21:32:36 +0800

    添加了motor_chassic.h文件

---
+++ MOTOR/motor_chassis.h
@@ -0,0 +1,21 @@
+#ifndef __MOTOR_CHASSIS_H
+#define __MOTOR_CHASSIS_H
+
+#include <stm32f4xx.h>
+
+typedef struct
+{
+    float    speed[4];
+    u16      angle[4];
+}data_3510_t;
+
+extern data_3510_t data_3510;
+
+
+void Get_3510_data(CanRxMsg rx_message);
+extern void Cmd_3510_ESC(int16_t current_201,int16_t current_202,int16_t current_203,int16_t current_204);
+
+
+
+
+#endif  
