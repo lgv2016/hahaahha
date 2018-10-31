4d6419817dd9110cba90d7e82bcbcd80b98bb395
lgv2016 <lgv2016@qq.com>
2018-11-01 01:10:42 +0800

    使用枚举量来代表数组索引

--- MOTOR/motor_cradle_head.c
+++ MOTOR/motor_cradle_head.c
@@ -2,30 +2,42 @@
 //所用驱动为2016年旧版电调
 #include <motor_cradle_head.h>
 
-static int16_t err_angle[2]={0};
-static int16_t last_angle[2]={0};
+static CanTxMsg s_tx_message;
 
 data_6623_t g_data_6623;
+data_2006_t g_data_2006;
 
 void Cmd_6623_ESC(int16_t  current_205,int16_t current_206)
 {
-    CanTxMsg tx_message;
+    s_tx_message.StdId = 0x1FF;
+    s_tx_message.IDE = CAN_Id_Standard;
+    s_tx_message.RTR = CAN_RTR_Data;
+    s_tx_message.DLC = 0x08;
     
-    tx_message.StdId = 0x1FF;
-    tx_message.IDE = CAN_Id_Standard;
-    tx_message.RTR = CAN_RTR_Data;
-    tx_message.DLC = 0x08;
-    
-    tx_message.Data[0] = (unsigned char)(current_205 >> 8);
-    tx_message.Data[1] = (unsigned char)current_205;
-    tx_message.Data[2] = (unsigned char)(current_206 >> 8);
-    tx_message.Data[3] = (unsigned char)current_206;
-    tx_message.Data[4] = 0x00;
-    tx_message.Data[5] = 0x00;
-    tx_message.Data[6] = 0x00;
-    tx_message.Data[7] = 0x00;
+    s_tx_message.Data[0] = (unsigned char)(current_205 >> 8);
+    s_tx_message.Data[1] = (unsigned char)current_205;
+    s_tx_message.Data[2] = (unsigned char)(current_206 >> 8);
+    s_tx_message.Data[3] = (unsigned char)current_206;
+
   
-    CAN_Transmit(CAN1,&tx_message);
+    CAN_Transmit(CAN1,&s_tx_message);
+}
+
+void Cmd_2006_ESC(int16_t  current_207)
+{
+     CanTxMsg s_tx_message;
+    
+    s_tx_message.StdId = 0x1FF;
+    s_tx_message.IDE = CAN_Id_Standard;
+    s_tx_message.RTR = CAN_RTR_Data;
+    s_tx_message.DLC = 0x08;
+    
+    s_tx_message.Data[4] = (unsigned char)(current_207 >> 8);
+    s_tx_message.Data[5] = (unsigned char)current_207;
+    s_tx_message.Data[6] = 0x00;
+    s_tx_message.Data[7] = 0x00;
+  
+    CAN_Transmit(CAN1,&s_tx_message);
 }
 
 void Get_6623_data(CanRxMsg rx_message)
@@ -34,17 +46,17 @@ void Get_6623_data(CanRxMsg rx_message)
     {
        case 0x205:
       {
-          g_data_6623.angle[0]             =   rx_message.Data[0]<<8|rx_message.Data[1];
-          g_data_6623.actual_current[0]    =   rx_message.Data[2]<<8|rx_message.Data[3];
-          g_data_6623.set_current[0]       =   rx_message.Data[4]<<8|rx_message.Data[5];
+          g_data_6623.angle[YAW]             =   rx_message.Data[0]<<8|rx_message.Data[1];
+          g_data_6623.actual_current[YAW]    =   rx_message.Data[2]<<8|rx_message.Data[3];
+          g_data_6623.set_current[YAW]       =   rx_message.Data[4]<<8|rx_message.Data[5];
 
           break;
       }
       case 0x206:
       {
-          g_data_6623.angle[1]             =   rx_message.Data[0]<<8|rx_message.Data[1];
-          g_data_6623.actual_current[1]    =   rx_message.Data[2]<<8|rx_message.Data[3];
-          g_data_6623.set_current[1]       =   rx_message.Data[4]<<8|rx_message.Data[5];
+          g_data_6623.angle[PITCH]             =   rx_message.Data[0]<<8|rx_message.Data[1];
+          g_data_6623.actual_current[PITCH]    =   rx_message.Data[2]<<8|rx_message.Data[3];
+          g_data_6623.set_current[PITCH]       =   rx_message.Data[4]<<8|rx_message.Data[5];
     
           break;
       }
@@ -53,30 +65,22 @@ void Get_6623_data(CanRxMsg rx_message)
     } 
 }
 
-void Get_6623_Speed(data_6623_t data)//M法 10ms
+void Get_2006_data(CanRxMsg rx_message)
 {
-
-    float temp;
-    err_angle[0]=data.angle[0]-last_angle[0];
-    
-    if((err_angle[0]<4000)&&(err_angle[0]>-4000))
+     switch(rx_message.StdId)
     {
-        temp=((err_angle[0]*360)/8191.0f)*100.0f;
-          if((temp<4000)&&(temp>-4000))
-            g_data_6623.speed[0]=temp;
-    }
-    last_angle[0]=data.angle[0];
-    
-    
-    err_angle[1]=data.angle[1]-last_angle[1];
-    
-     if((err_angle[1]<4000)&&(err_angle[1]>-4000))
-    {
-        temp=((err_angle[1]*360)/8191.0f)*100.0f;
-          if((temp<4000)&&(temp>-4000))
-            g_data_6623.speed[1]   =   temp;
-    }
-    last_angle[1]=g_data_6623.angle[1];
+      case 0x207:
+      {
+          
+          g_data_2006.angle               =   rx_message.Data[0]<<8|rx_message.Data[1];
+          g_data_2006.speed               =   rx_message.Data[2]<<8|rx_message.Data[3];
+          g_data_2006.torque              =   rx_message.Data[4]<<8|rx_message.Data[5];
+          
+          break;
+      }
+      default:
+        break;
+    } 
 }
 
 
--- MOTOR/motor_cradle_head.h
+++ MOTOR/motor_cradle_head.h
@@ -2,27 +2,48 @@
 #define __BSP_MOTOR_CRADLE_HEAD_H
 
 #include <stm32f4xx.h>
+enum
+{
+    YAW,
+    PITCH,
+    NUM_6623
+};
 
 typedef struct
 {
-    float    speed[2];
-    
-    u16      angle[2];
-    
-    int16_t  set_current[2];
-    
-    int16_t  actual_current[2];
+    float    speed[NUM_6623];
+    u16      angle[NUM_6623];
+    int16_t  set_current[NUM_6623];
+    int16_t  actual_current[NUM_6623];
     
 }data_6623_t;
 
 
+typedef struct
+{
+    u16        angle;
+    int16_t    speed;
+    int16_t    torque;
+    
+}data_2006_t;
+
+
 
 extern data_6623_t g_data_6623;
+extern data_2006_t g_data_2006;
+
 
 
 extern void Cmd_6623_ESC(int16_t  current_205,int16_t current_206);
 extern void Get_6623_data(CanRxMsg rx_message);
-extern void Get_6623_Speed(data_6623_t g_data_6623);//бу/s
+
+
+
+
+extern void Cmd_2006_ESC(int16_t  current_207);
+extern void Get_2006_data(CanRxMsg rx_message);
+
+
 
 
 #endif  
