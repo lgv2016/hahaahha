a5f5c495af35d08d1c67eecd3b306759f8bcf1a9
lgv <lgv2016@qq.com>
2018-11-18 15:06:29 +0800

    添加了摩擦轮

--- BSP/bsp_time.c
+++ BSP/bsp_time.c
@@ -17,8 +17,7 @@ void BSP_TIME_Init()
     TIM_Cmd(TIM6, ENABLE);
     
 
-    
-    //TIM5 OC4  
+    //TIM5 OC4  OC3
     TIM_TimeBase_InitStructure.TIM_ClockDivision    =   TIM_CKD_DIV1;
     TIM_TimeBase_InitStructure.TIM_CounterMode      =   TIM_CounterMode_Up;
     TIM_TimeBase_InitStructure.TIM_Period           =   20000-1;
@@ -29,9 +28,14 @@ void BSP_TIME_Init()
     TIM_OCInitStructure.TIM_OCPolarity              =   TIM_OCPolarity_High;
     TIM_OCInitStructure.TIM_Pulse                   =   0;
     TIM_OCInitStructure.TIM_OutputState             =   TIM_OutputState_Enable;
+    
     TIM_OC4Init(TIM5,&TIM_OCInitStructure);
+    TIM_OC3Init(TIM5,&TIM_OCInitStructure);
     
     TIM_OC4PreloadConfig(TIM5, TIM_OCPreload_Enable);
-    TIM_ARRPreloadConfig(TIM5,DISABLE);	
-
+    TIM_OC3PreloadConfig(TIM5, TIM_OCPreload_Enable);
+    
+    TIM_ARRPreloadConfig(TIM5,ENABLE);	
+    TIM_Cmd(TIM5, DISABLE);
+    
 }
