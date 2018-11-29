6ad092b481897a73a6b206b988a1c3aa91182023
lgv <lgv2016@qq.com>
2018-11-29 22:18:01 +0800

    添加了串口数据解算任务

---
+++ TASK/task_usart_parse.c
@@ -0,0 +1,12 @@
+#include <task_usart_parse.h>
+#include <drive_delay.h>
+
+void vTaskUSARTParse(void *pvParameters)
+{
+    while(1)
+    {
+		vTaskDelay(100);
+	//	printf("ff\r\n");
+		
+    }
+}
---
+++ TASK/task_usart_parse.h
@@ -0,0 +1,11 @@
+#ifndef __TASK_UASRT_PARSE_H
+#define __TASK_UASRT_PARSE_H
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+extern void vTaskUSARTParse(void *pvParameters);
+
+
+#endif
+
