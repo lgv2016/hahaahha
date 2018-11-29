72619efd0512557635d1200b52d91a01bc8e75e2
lgv <lgv2016@qq.com>
2018-11-29 22:19:02 +0800

    添加了CAN数据解算任务

---
+++ TASK/task_can_parse.c
@@ -0,0 +1,17 @@
+#include <task_can_parse.h>
+#include <drive_delay.h>
+#include <motor_cradle_head.h>
+#include <motor_chassis.h>
+
+
+
+void vTaskCANParse(void *pvParameters)
+{
+    while(1)
+    {
+		ulTaskNotifyTake(pdTRUE,portMAX_DELAY);
+		Get_6623_data(s_rx_message);
+        Get_2006_data(s_rx_message);
+        Get_3510_data(s_rx_message);   
+    }
+}
---
+++ TASK/task_can_parse.h
@@ -0,0 +1,13 @@
+#ifndef __TASK_CAN_PARSE_H
+#define __TASK_CAN_PARSE_H
+#include "FreeRTOS.h"
+#include "task.h"
+#include <stm32f4xx.h>
+
+extern void vTaskCANParse(void *pvParameters);
+
+
+#endif
+
+
+
