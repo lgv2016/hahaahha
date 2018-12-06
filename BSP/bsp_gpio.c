20af180f00ba0d691112904009262cf6f6d974e1
lgv <lgv2016@qq.com>
2018-12-06 17:52:10 +0800

    添加了mpu6500和摩擦轮的控制IO

--- BSP/bsp_gpio.c
+++ BSP/bsp_gpio.c
@@ -1,4 +1,5 @@
 #include <bsp_gpio.h>
+#include <bsp_iic.h>
 
 void  BSP_GPIO_Init(void)
 {
@@ -11,14 +12,32 @@ void  BSP_GPIO_Init(void)
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG,ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOH,ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOI,ENABLE);
+	
+	//RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);//使用外部中断
     
-    //IMU 模拟IIC
-    GPIO_InitStructure.GPIO_Pin    =  GPIO_Pin_6 | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9;   
-    GPIO_InitStructure.GPIO_Mode   =  GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType  =  GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_PuPd   =  GPIO_PuPd_UP;
-    GPIO_InitStructure.GPIO_Speed  =  GPIO_Speed_100MHz;
+    //IMU 模拟IIC    
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;   
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_UP;
+    GPIO_InitStructure.GPIO_Speed=GPIO_Speed_100MHz;
     GPIO_Init(GPIOF, &GPIO_InitStructure);
+	
+	GPIO_SetBits(GPIOF, GPIO_Pin_6); //CS=1
+	GPIO_SetBits(GPIOF, GPIO_Pin_9); //IIC_SDA=1
+	GPIO_SetBits(GPIOF, GPIO_Pin_7);//IIC_SCL=1
+	GPIO_ResetBits(GPIOF, GPIO_Pin_8); //AD=0
+	
+	
+
+	//MPU6500 INT
+	//PB8
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	//SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOB,EXTI_PinSource8);
+	
     
     //LED   
     //PG1
@@ -28,7 +47,7 @@ void  BSP_GPIO_Init(void)
     GPIO_InitStructure.GPIO_PuPd   =  GPIO_PuPd_UP;
     GPIO_InitStructure.GPIO_Speed  =  GPIO_Speed_100MHz;
     GPIO_Init(GPIOG, &GPIO_InitStructure);
-    GPIO_SetBits(GPIOG, GPIO_Pin_1);     //关闭
+    GPIO_ResetBits(GPIOG, GPIO_Pin_1);     //关闭
     
     //TIM5 OC4  OC3
     //PI0  PH12
@@ -88,5 +107,16 @@ void  BSP_GPIO_Init(void)
     GPIO_Init(GPIOB, &GPIO_InitStructure);
     GPIO_PinAFConfig(GPIOB,GPIO_PinSource13,GPIO_AF_CAN2);
     GPIO_PinAFConfig(GPIOB,GPIO_PinSource12,GPIO_AF_CAN2);
-    
+	
+	
+	//TIM3 OC2  
+    //PB5
+    GPIO_InitStructure.GPIO_Pin    =  GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Mode   =  GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType  =  GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd   =  GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed  =  GPIO_Speed_100MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    GPIO_PinAFConfig(GPIOB,GPIO_PinSource5, GPIO_AF_TIM3);
+
 }
