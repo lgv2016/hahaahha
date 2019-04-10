f417f1b1fa788a956ac2e4a55c7fa50770858f62
lgv <lgv2016@qq.com>
2019-04-10 12:12:21 +0800

    添加了云台控制文件

--- DRIVE/drive_gimble.c
+++ DRIVE/drive_gimble.c
@@ -1,29 +1,26 @@
 #include <drive_gimble.h>
-
-
-
 #include <drive_control.h>
-
-#include <drive_delay.h> 
-#include <drive_imu.h>
-#include <motor_chassis.h>
 #include <motor_cradle_head.h>
 #include <robotstatus.h>
 #include <drive_rc.h>
 
 
-#define YAW_INIT_ANGLE    281.0f
-#define PIT_INIT_ANGLE    110.0f
-
-
-
-
 void GIMBLE_Control()
 {
-	if( robot_status.gimbal_status==INIT_GOOD)
+	if(robot_status.gimbal_mode==MANUAL)
 	{
-		g_angle_target.pitch =PIT_INIT_ANGLE+g_pit_target;
-		g_angle_target.yaw   =YAW_INIT_ANGLE+g_yaw_target;
-	}
-    Angle_6623_Control(g_angle_target);
+		if(robot_status.gimbal_status==NO_INIT)
+		{
+			g_angle_target.pitch = PIT_INIT_ANGLE;
+			g_angle_target.yaw   = YAW_INIT_ANGLE;
+		}
+		
+		if(robot_status.gimbal_status==INIT_GOOD)
+		{
+			g_angle_target.pitch = PIT_INIT_ANGLE+g_pit_target;
+			g_angle_target.yaw   = g_yaw_target;
+		}
+    }
+   Angle_6623_Control(g_angle_target);
 }
+
--- DRIVE/drive_gimble.h
+++ DRIVE/drive_gimble.h
@@ -5,4 +5,20 @@
 
 #include <stm32f4xx.h>
 
+typedef struct
+{
+	float input;
+	float out;
+	float max;
+	float min;
+	float period;
+} fric_t;
+
+extern fric_t fric1;
+extern fric_t fric2;
+
+extern u8 auto_flag;
+
+extern void GIMBLE_Control(void);
+
 #endif
