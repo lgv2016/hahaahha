50beab743bab8357468e809951da3b1a9a18f168
lgv <lgv2016@qq.com>
2019-04-10 12:07:27 +0800

    添加UART8 dma

--- BSP/bsp_dma.c
+++ BSP/bsp_dma.c
@@ -3,10 +3,14 @@
 u8  g_DMA_Dbus_Buff[DMA_Dbus_Buff_Size];
 u8  g_DMA_Judge_Send_Buff[DMA_Judge_Send_Buff_Size];
 u8  g_DMA_Judge_Reve_Buff[DMA_Judge_Reve_Buff_Size];
+u8  g_DMA_MiniPC_Reve_Buff[DMA_MiniPC_Reve_Buff_Size];
+u8  g_DMA_MiniPC_Send_Buff[DMA_MiniPC_Send_Buff_Size];
 
 void BSP_DMA_Init()
 {
     DMA_InitTypeDef DMA_InitStructure;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1,ENABLE);
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2,ENABLE);
     
     //USART1_RX  DMA2 数据流2通道4
@@ -70,5 +74,48 @@ void BSP_DMA_Init()
     DMA_InitStructure.DMA_Priority                = DMA_Priority_Medium;
     DMA_Init(DMA2_Stream1,&DMA_InitStructure);
     DMA_Cmd(DMA2_Stream1, ENABLE);
+	
+	
+	//UART8_TX  DMA1 数据流0通道5
+    DMA_DeInit(DMA1_Stream0); 
+	
+    while (DMA_GetCmdStatus(DMA1_Stream0));
+    DMA_InitStructure.DMA_Channel                 = DMA_Channel_5;
+    DMA_InitStructure.DMA_PeripheralBaseAddr      = (u32)&UART8->DR;
+    DMA_InitStructure.DMA_Memory0BaseAddr         = (u32)g_DMA_MiniPC_Send_Buff;
+    DMA_InitStructure.DMA_DIR                     = DMA_DIR_MemoryToPeripheral;
+    DMA_InitStructure.DMA_BufferSize              = DMA_MiniPC_Send_Buff_Size;
+    DMA_InitStructure.DMA_PeripheralInc           = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_MemoryInc               = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_PeripheralDataSize      = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryDataSize          = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_Mode                    = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_Priority                = DMA_Priority_Medium;
+    DMA_InitStructure.DMA_FIFOMode                = DMA_FIFOMode_Disable;
+    DMA_InitStructure.DMA_FIFOThreshold           = DMA_FIFOThreshold_Full;
+    DMA_InitStructure.DMA_MemoryBurst             = DMA_MemoryBurst_Single;
+    DMA_InitStructure.DMA_PeripheralBurst         = DMA_PeripheralBurst_Single; 
+    DMA_Init(DMA1_Stream0,&DMA_InitStructure);
+  
+    //UART8_RX  DMA1 数据流6通道5
+    DMA_DeInit(DMA1_Stream6);       
+    while (DMA_GetCmdStatus(DMA1_Stream6));
+    DMA_InitStructure.DMA_BufferSize              = DMA_MiniPC_Reve_Buff_Size;
+    DMA_InitStructure.DMA_Channel                 = DMA_Channel_5;
+    DMA_InitStructure.DMA_DIR                     = DMA_DIR_PeripheralToMemory ;
+    DMA_InitStructure.DMA_FIFOMode                = DMA_FIFOMode_Disable;
+    DMA_InitStructure.DMA_FIFOThreshold           = DMA_FIFOThreshold_Full;
+    DMA_InitStructure.DMA_Memory0BaseAddr         = (u32)g_DMA_MiniPC_Reve_Buff;
+    DMA_InitStructure.DMA_MemoryBurst             = DMA_MemoryBurst_Single;
+    DMA_InitStructure.DMA_MemoryDataSize          = DMA_MemoryDataSize_Byte;
+    DMA_InitStructure.DMA_MemoryInc               = DMA_MemoryInc_Enable;
+    DMA_InitStructure.DMA_Mode                    = DMA_Mode_Normal;
+    DMA_InitStructure.DMA_PeripheralBaseAddr      = (u32)&(UART8->DR);
+    DMA_InitStructure.DMA_PeripheralBurst         = DMA_PeripheralBurst_Single;
+    DMA_InitStructure.DMA_PeripheralDataSize      = DMA_PeripheralDataSize_Byte;
+    DMA_InitStructure.DMA_PeripheralInc           = DMA_PeripheralInc_Disable;
+    DMA_InitStructure.DMA_Priority                = DMA_Priority_Medium;
+    DMA_Init(DMA1_Stream6,&DMA_InitStructure);
+    DMA_Cmd(DMA1_Stream6, ENABLE);
     
 }
--- BSP/bsp_dma.h
+++ BSP/bsp_dma.h
@@ -7,9 +7,15 @@
 #define DMA_Judge_Send_Buff_Size 255
 #define DMA_Judge_Reve_Buff_Size 255
 
+
+#define DMA_MiniPC_Send_Buff_Size 20
+#define DMA_MiniPC_Reve_Buff_Size 20
+
 extern u8 g_DMA_Dbus_Buff[DMA_Dbus_Buff_Size];
 extern u8 g_DMA_Judge_Send_Buff[DMA_Judge_Send_Buff_Size];
 extern u8 g_DMA_Judge_Reve_Buff[DMA_Judge_Reve_Buff_Size];
+extern u8 g_DMA_MiniPC_Reve_Buff[DMA_MiniPC_Reve_Buff_Size];
+extern u8 g_DMA_MiniPC_Send_Buff[DMA_MiniPC_Send_Buff_Size];
 
 extern void BSP_DMA_Init(void);
 #endif  
