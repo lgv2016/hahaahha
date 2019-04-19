4467f7f3f7ef4fc5e3ca85c1fc5cd8c722213251
lgv <lgv2016@qq.com>
2019-04-19 12:47:02 +0800

    添加ID:209为云台控制板发送数据到主控ID

--- TASK/task_can_parse.c
+++ TASK/task_can_parse.c
@@ -3,6 +3,8 @@
 #include <motor_cradle_head.h>
 #include <motor_chassis.h>
 
+#include <drive_shoot.h>
+
 
 
 void vTaskCANParse(void *pvParameters)
@@ -14,6 +16,8 @@ void vTaskCANParse(void *pvParameters)
         Get_2006_data(s_rx_message);
         Get_3510_data(s_rx_message); 
 		Get_GIMBLE_data(s_rx_message);
+		
+		GET_209_Data(s_rx_message);
     }
 }
 
